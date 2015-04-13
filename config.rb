180ccfcfbfc31dd85bfc33b00ddab6b60175d7ba
Sean C Davis <scdavis41@gmail.com>
2015-04-13 11:07:33 -0400

    config file

--- config.rb
+++ config.rb
@@ -36,9 +36,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -52,6 +52,10 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
+
+set :fonts_dir, 'fonts'
+
+activate :directory_indexes
 
 # Build-specific configuration
 configure :build do
